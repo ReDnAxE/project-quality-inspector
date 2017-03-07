9d7352c4aa4323e80acb74290d19c1a888f0fdcc
Alexandre GESLIN <alexandre@gesl.in>
2017-03-07 10:17:07 +0100

    maj version

--- src/Application/ConsoleApplication.php
+++ src/Application/ConsoleApplication.php
@@ -31,7 +31,7 @@ class ConsoleApplication
      */
     public function run()
     {
-        $application = new Application('pqi', '1.0.4');
+        $application = new Application('pqi', '1.1.0');
         $command = new MainCommand();
         $application->add($command);
         $application->setDefaultCommand($command->getName(), true);
